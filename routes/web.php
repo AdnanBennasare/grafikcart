0d9d7cbb175dd6118b4c9a306ff7702d147caa08
ADNAN <bennassar.adnan.solicode@gmail.com>
2023-10-17 16:28:42 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 //     return "bonjour";
 // });
 
-// http://127.0.0.1:8000/blog?name=jhon
+// // http://127.0.0.1:8000/blog?name=jhon
 // Route::get('/blog', function () {
 //     return [
 //         "name" => $_GET['name'],
@@ -33,7 +33,7 @@ Route::get('/', function () {
 
 
 
-// http://127.0.0.1:8000/blog?name=jhon&age=9
+// // http://127.0.0.1:8000/blog?name=jhon&age=9
 
 // Route::get('/blog', function (Request $request) {
 //     return [
@@ -45,26 +45,49 @@ Route::get('/', function () {
 
 
 // http://127.0.0.1:8000/blog/fdsfsf-1
-// Route::get('/blog/{slug}-{id}', function (string $slug, string $id) {
+
+// Route::get('/blog', function (Request $request) {
+//     return [
+//         "link" => \route('blog.show', ['slug' => 'article', 'id' => 13]),
+//     ] ;
+// })->name('blog.index');
+
+// Route::get('/blog/{slug}/{id}', function (Request $request, string $slug, string $id) {
 //     return [
 //         "slug" => $slug,
-//         "id" => "$id"
-
+//         "id" => $id,
+//         "name" => $request->input('name'),
 //     ];
 // })->where([
 
 //     'id' => '[0-9]+',
 //     'slug' => '[a-z0-9\-]+'
 
-// ]);
+// ])->name('blog.show');
 
 
-
-
-Route::get('/blog', function (Request $request) {
-    return [
-        "name" => $request->all(),
-        "Article" => "1 article"
-
-    ] ;
+Route::prefix('/blog')->name('blog.')->group(function (){
+    
+    Route::get('/', function (Request $request) {
+        return [
+            "link" => \route('blog.show', ['slug' => 'article', 'id' => 13]),
+        ] ;
+    })->name('index');
+    
+    Route::get('/{slug}/{id}', function (Request $request, string $slug, string $id) {
+        return [
+            "slug" => $slug,
+            "id" => $id,
+            "name" => $request->input('name'),
+        ];
+    })->where([
+    
+        'id' => '[0-9]+',
+        'slug' => '[a-z0-9\-]+'
+    
+    ])->name('show');
 });
+
+
+
+
